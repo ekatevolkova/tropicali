15175614f6e699f77a2f52f84e0924eb5af44631
Ekaterina Volkova <ekatevolkova@gmail.com>
2020-08-19 21:24:04 +0200

    optimized version of the landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,58 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCss = require('gulp-clean-css');
+var sourcemaps = require("gulp-sourcemaps");
+
+var browserSync = require('browser-sync').create()
+
+var imagemin = require("gulp-imagemin")
 
 sass.compiler = require('node-sass');
 
 gulp.task("sass", function () {
     //we want to run "sass css/app.scss app.css --watch"
-    return gulp.src("css/app.scss")
+    return gulp.src("src/css/app.scss")
+        .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(cleanCss())
-        .pipe(gulp.dest("."))
+        .pipe(
+            cleanCss({
+                compatibility: 'ie8'
+            })
+            )
+            .pipe(sourcemaps.write())
+        .pipe(gulp.dest("dist"))
+        .pipe(browserSync.stream())
 });
 
+gulp.task("html", function () {
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest("dist"))
+})
+
+gulp.task("fonts", function () {
+    return gulp.src("src/fonts/*")
+    .pipe(gulp.dest("dist/fonts"))
+})
+
+gulp.task("images", function () {
+    return gulp.src("src/img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/img"))
+})
+
 gulp.task("watch", function () {
-    gulp.watch("css/app.scss", gulp.series("sass"))
+
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    })
+    gulp.watch("src/*.html", gulp.series("html")).on("change", browserSync.reload)
+    gulp.watch("src/css/app.scss", gulp.series("sass"))
+    gulp.watch("src/fonts/*", gulp.series("fonts"))
+    gulp.watch("src/img/*", gulp.series("images"))
 })
 
 
-gulp.task('default', gulp.series("sass", "watch"));
+gulp.task('default', gulp.series("html", "sass", "fonts", "images", "watch"));
  
