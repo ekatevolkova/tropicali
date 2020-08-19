e7c68b2cda9f3fa0a5f47edffa8bf8acc61291d0
Ekaterina Volkova <ekatevolkova@gmail.com>
2020-08-19 17:03:33 +0200

    changes from the last lesson

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    // we wnt to run "sass css/app.scss app.css --watch'
-    cb();
-  }
-  
-  exports.default = defaultTask
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCss = require('gulp-clean-css');
+
+sass.compiler = require('node-sass');
+
+gulp.task("sass", function () {
+    //we want to run "sass css/app.scss app.css --watch"
+    return gulp.src("css/app.scss")
+        .pipe(sass())
+        .pipe(cleanCss())
+        .pipe(gulp.dest("."))
+});
+
+gulp.task("watch", function () {
+    gulp.watch("css/app.scss", gulp.series("sass"))
+})
+
+
+gulp.task('default', gulp.series("sass", "watch"));
+ 
